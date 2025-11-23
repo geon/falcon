66590bc0aafefa4509c2ce64b0960f3b4c9c7663
Victor Widell <victor.widell@gmail.com>
2025-11-23 19:11:49 +0100

    Test: parseDiceRollRow > match

--- src/parsers/parseDiceRollRow.ts
+++ src/parsers/parseDiceRollRow.ts
@@ -1,5 +1,17 @@
 import { parseWithErrorMessage } from "../parser-combinators/combinators/parseWithErrorMessage.js";
-import { parseError } from "../parser-combinators/combinators/parseError.js";
+import { parseSequence } from "../parser-combinators/combinators/parseSequence.js";
+import { parseEol } from "../parser-combinators/combinators/parseEol.js";
+import { parseString } from "../parser-combinators/combinators/parseString.js";
+import { parseOptional } from "../parser-combinators/combinators/parseOptional.js";
+import { parseChar } from "../parser-combinators/combinators/parseChar.js";
+import { parseDigit } from "../parser-combinators/combinators/parseDigit.js";
+import { parseMonad } from "../parser-combinators/combinators/parseMonad.js";
+import { parseOneOrMore } from "../parser-combinators/combinators/parseSome.js";
+
+const parseNumber = parseMonad(
+	parseOneOrMore(parseDigit(10)),
+	(parsed, { result }) => result(parseInt(parsed.join(""))),
+);
 
 export interface DiceRollRow {
 	readonly scores: readonly number[];
@@ -8,5 +20,25 @@ export interface DiceRollRow {
 
 export const parseDiceRollRow = parseWithErrorMessage(
 	"Expected dice roll row.",
-	parseError,
+	parseMonad(
+		parseSequence([
+			parseString("If you score"),
+			parseOptional(parseChar("d")),
+			parseChar(" "),
+			parseMonad(
+				parseSequence([parseNumber, parseChar("-"), parseNumber]),
+				([from, , to], { result }) => result({ from, to }),
+			),
+			parseString(", turn to "),
+			parseNumber,
+			parseEol,
+		]),
+		([, , , dice, , link], { result }) =>
+			result<DiceRollRow>({
+				scores: Array(dice.to - dice.from + 1)
+					.fill(0)
+					.map((_, i) => dice.from + i),
+				link,
+			}),
+	),
 );
--- src/parsers/tests/parseDiceRollRow.test.ts
+++ src/parsers/tests/parseDiceRollRow.test.ts
@@ -1,4 +1,7 @@
-import { createParseError } from "../../parser-combinators/combinators/Parser.js";
+import {
+	createParseError,
+	createParseResult,
+} from "../../parser-combinators/combinators/Parser.js";
 import { testExamples } from "../../parser-combinators/combinators/tests/testExamples.js";
 import { parseDiceRollRow } from "../parseDicerollRow.js";
 
@@ -8,5 +11,11 @@ testExamples("parseDiceRollRow", [
 		parser: parseDiceRollRow,
 		input: "-",
 		result: createParseError(0, "Expected dice roll row."),
+	},
+	{
+		name: "match",
+		parser: parseDiceRollRow,
+		input: "If you score 1-3, turn to 72\n",
+		result: createParseResult(28, { scores: [1, 2, 3], link: 72 }),
 	},
 ]);
