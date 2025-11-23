719de6ce6b8df2f6667ba261d32983e1fe33f75f
Victor Widell <victor.widell@gmail.com>
2025-11-23 20:51:31 +0100

    Test: parseChoices > match

--- src/parsers/parseChoices.ts
+++ src/parsers/parseChoices.ts
@@ -1,7 +1,79 @@
-import { parseError } from "../parser-combinators/combinators/parseError.js";
-import { parseWithErrorMessage } from "../parser-combinators/combinators/parseWithErrorMessage.js";
+import { parseAnyCharBut } from "../parser-combinators/combinators/parseAnyCharBut.js";
+import { parseDigit } from "../parser-combinators/combinators/parseDigit.js";
+import { parseMonad } from "../parser-combinators/combinators/parseMonad.js";
+import { parseNewline } from "../parser-combinators/combinators/parseNewline.js";
+import {
+	createParseResult,
+	type Parser,
+	type ParseResult,
+} from "../parser-combinators/combinators/Parser.js";
+import { parseSequenceIndex } from "../parser-combinators/combinators/parseSequenceIndex.js";
+import { parseOneOrMore } from "../parser-combinators/combinators/parseSome.js";
+import { parseString } from "../parser-combinators/combinators/parseString.js";
 
-export const parseChoices = parseWithErrorMessage(
-	"Expected choices.",
-	parseError,
+const parseNumber = parseMonad(
+	parseOneOrMore(parseDigit(10)),
+	(parsed, { result }) => result(parseInt(parsed.join(""))),
 );
+
+const parseTurnInstruction = parseSequenceIndex(1, [
+	parseString("Turn to "),
+	parseNumber,
+]);
+
+interface Option {
+	readonly choice: string;
+	readonly link: number;
+}
+
+const newline = "\n";
+export const parseChoices: Parser<readonly Option[]> = (
+	input,
+	fromIndex,
+): ParseResult<readonly Option[]> => {
+	let consumed = 0;
+	let choices: string[] = [];
+	let options: Option[] = [];
+
+	for (;;) {
+		const parsed = parseSequenceIndex(0, [parseUntil(newline), parseNewline])(
+			input,
+			fromIndex + consumed,
+		);
+		if (parsed.type === "error") {
+			return parsed;
+		}
+		consumed += parsed.consumed;
+		choices.push(parsed.parsed);
+
+		if (parseTurnInstruction(input, fromIndex + consumed).type !== "error") {
+			break;
+		}
+	}
+
+	for (const choice of choices) {
+		const parsed = parseSequenceIndex(0, [parseTurnInstruction, parseNewline])(
+			input,
+			fromIndex + consumed,
+		);
+		if (parsed.type === "error") {
+			return parsed;
+		}
+
+		consumed += parsed.consumed;
+
+		options.push({
+			choice,
+			link: parsed.parsed,
+		});
+	}
+
+	return createParseResult(consumed, options);
+};
+
+function parseUntil(endChar: string): Parser<string> {
+	return parseMonad(
+		parseOneOrMore(parseAnyCharBut(endChar)),
+		(parsed, { result }) => result(parsed.join("")),
+	);
+}
--- src/parsers/tests/parseChoices.test.ts
+++ src/parsers/tests/parseChoices.test.ts
@@ -1,4 +1,7 @@
-import { createParseError } from "../../parser-combinators/combinators/Parser.js";
+import {
+	createParseError,
+	createParseResult,
+} from "../../parser-combinators/combinators/Parser.js";
 import { testExamples } from "../../parser-combinators/combinators/tests/testExamples.js";
 import { parseChoices } from "../parseChoices.js";
 
@@ -7,6 +10,25 @@ testExamples("parseChoices", [
 		name: "no match",
 		parser: parseChoices,
 		input: "-",
-		result: createParseError(0, "Expected choices."),
+		result: createParseError(1, "Expected newline."),
+	},
+	{
+		name: "match",
+		parser: parseChoices,
+		input:
+			"Duck into a doorway and wait to confront him at close quarters?\nSlip through the Intelfax warehouse and lose your pursuer in the lowlife area, Old Geneva?\nBlast him?\nTurn to 3\nTurn to 10\nTurn to 29\n",
+		result: createParseResult(198, [
+			{
+				choice:
+					"Duck into a doorway and wait to confront him at close quarters?",
+				link: 3,
+			},
+			{
+				choice:
+					"Slip through the Intelfax warehouse and lose your pursuer in the lowlife area, Old Geneva?",
+				link: 10,
+			},
+			{ choice: "Blast him?", link: 29 },
+		]),
 	},
 ]);
