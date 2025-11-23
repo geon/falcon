6b6165445712118ee685cce95aaf7d5574d2c27d
Victor Widell <victor.widell@gmail.com>
2025-11-23 19:16:02 +0100

    Test: parseDiceRoll > match

--- src/parsers/tests/parseDiceRoll.test.ts
+++ src/parsers/tests/parseDiceRoll.test.ts
@@ -1,4 +1,7 @@
-import { createParseError } from "../../parser-combinators/combinators/Parser.js";
+import {
+	createParseError,
+	createParseResult,
+} from "../../parser-combinators/combinators/Parser.js";
 import { testExamples } from "../../parser-combinators/combinators/tests/testExamples.js";
 import { parseDiceRoll } from "../parseDiceRoll.js";
 
@@ -8,5 +11,14 @@ testExamples("parseDiceRoll", [
 		parser: parseDiceRoll,
 		input: "-",
 		result: createParseError(0, "Expected dice roll row."),
+	},
+	{
+		name: "match",
+		parser: parseDiceRoll,
+		input: "If you score 1-3, turn to 72\nIf you score 4-6, turn to 91\n",
+		result: createParseResult(58, [
+			{ scores: [1, 2, 3], link: 72 },
+			{ scores: [4, 5, 6], link: 91 },
+		]),
 	},
 ]);
